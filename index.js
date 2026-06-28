c1a05306e2653584de2ce50383c0a1338bb4d053
khushi1911 <khushikaushik2030@gmail.com>
2026-06-28 20:52:37 +0530

    Update index.js
    comment

--- index.js
+++ index.js
@@ -18,20 +18,3 @@ app.listen(port ,() => {
   console.log(`server running on port ${port}`);
 });
 
-
-
-
-/* import express from "express";
-const app = express();
-const port = 3000;
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
-});
-
-
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
-*/ 
